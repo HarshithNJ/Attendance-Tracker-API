590cc30d1540218061d8b015073c1bc571515666
Harshith N J <njharshith122@gmail.com>
2025-01-09 20:08:09 +0530

    save record scripted

--- src/main/java/org/attendance/attendance_tracker/controller/attendanceController.java
+++ src/main/java/org/attendance/attendance_tracker/controller/attendanceController.java
@@ -1,13 +1,26 @@
 package org.attendance.attendance_tracker.controller;
 
+import org.attendance.attendance_tracker.dto.attendanceRecord;
 import org.attendance.attendance_tracker.service.attendanceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 public class attendanceController {
 
     @Autowired
     attendanceService service;
+
+    /*To Save Attendance Record*/
+    //To save Single Attendance Record
+    @GetMapping("/attendance")
+    public ResponseEntity<Object> saveAttendance(@RequestBody attendanceRecord attendance){
+        return service.saveAttendance(attendance);
+    }
+    
     
 }
--- src/main/java/org/attendance/attendance_tracker/service/attendanceService.java
+++ src/main/java/org/attendance/attendance_tracker/service/attendanceService.java
@@ -1,7 +1,13 @@
 package org.attendance.attendance_tracker.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.attendance.attendance_tracker.dto.attendanceRecord;
 import org.attendance.attendance_tracker.repository.attendanceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,5 +15,24 @@ public class attendanceService {
 
     @Autowired
     attendanceRepository repository;
+
+    public ResponseEntity<Object> saveAttendance(attendanceRecord attendance) {
+        if (repository.existByRollNoAndDate(attendance.getRollNO(), attendance.getDate())) {
+            
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("error", "Data Already Exists");
+
+            return new ResponseEntity<Object>(map, HttpStatus.BAD_REQUEST);
+        }else{
+
+            repository.save(attendance);
+
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("success", "Attendance marked for " + attendance.getName());
+            map.put("Data", attendance);
+
+            return new ResponseEntity<Object>(map, HttpStatus.CREATED);
+        }
+    }
     
 }
