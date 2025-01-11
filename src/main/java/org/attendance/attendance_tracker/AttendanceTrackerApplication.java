e59671944a1ce3adf46d1eb9102d7d48b034d57e
Harshith N J <njharshith122@gmail.com>
2025-01-11 11:20:11 +0530

    swagger dependency added

--- src/main/java/org/attendance/attendance_tracker/AttendanceTrackerApplication.java
+++ src/main/java/org/attendance/attendance_tracker/AttendanceTrackerApplication.java
@@ -3,7 +3,13 @@ package org.attendance.attendance_tracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Attendance Tracker API", version = "1.0", description = "API for Attendance Tracker", 
+					contact = @Contact(name = "Harshith N J", email = "Harshith.dev2024@outlook.com")))
 public class AttendanceTrackerApplication {
 
 	public static void main(String[] args) {
